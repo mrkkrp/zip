6bce0ee7a8158cfa1e2ff77728509e03df24d6a7
mrkkrp <markkarpov@opmbx.org>
2016-01-28 16:09:09 +0600

    Initial decisions on dependencies and structure

---
+++ Codec/Archive/Zip/Internal.hs
@@ -0,0 +1,14 @@
+-- |
+-- Module      :  Codec.Archive.Zip.Internal
+-- Copyright   :  © 2016 Mark Karpov
+-- License     :  BSD 3 clause
+--
+-- Maintainer  :  Mark Karpov <markkarpov@openmailbox.org>
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- TODO Describe me, please.
+
+module Codec.Archive.Zip.Internal
+  (  )
+where
