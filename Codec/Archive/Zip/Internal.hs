2a54029253a197e5e47f361a8dd49037e7baf133
mrkkrp <markkarpov@opmbx.org>
2016-02-10 16:50:26 +0600

    Minor changes and documentation in ‘Internal.hs’

--- Codec/Archive/Zip/Internal.hs
+++ Codec/Archive/Zip/Internal.hs
@@ -40,57 +40,104 @@ import qualified Data.Text.Encoding as T
 -- archive.
 
 data PendingAction
-  = DeleteArchive (Path Abs File)
-    -- ^ Delete archive file if it exists
-  | AddEntry CompressionMethod ByteString EntrySelector
+  = AddEntry CompressionMethod ByteString EntrySelector
+    -- ^ Add entry given its binary content as strict 'ByteString'
   | SinkEntry CompressionMethod (Source (ResourceT IO) ByteString) EntrySelector
+    -- ^ Add entry given its 'Source'
   | LoadEntry CompressionMethod (Path Abs File) EntrySelector
+    -- ^ Load entry from file on disk
   | CopyEntry (Path Abs File) EntrySelector
+    -- ^ Copy an entry form another archive without re-compression
   | RenameEntry EntrySelector EntrySelector
+    -- ^ Change name the entry inside archive
   | DeleteEntry EntrySelector
+    -- ^ Delete entry from archive
   | Recompress CompressionMethod EntrySelector
+    -- ^ Change compression method on an entry
   | SetEntryComment Text EntrySelector
+    -- ^ Set comment for a particular entry
   | DeleteEntryComment EntrySelector
+    -- ^ Delete comment of particular entry
   | SetModTime UTCTime EntrySelector
+    -- ^ Set modification time of particular entry
   | AddExtraField ExtraField EntrySelector
+    -- ^ Add an extra field to specified entry
   | DeleteExtraField Natural EntrySelector
+    -- ^ Delete an extra filed of specified entry
   | SetArchiveComment Text
+    -- ^ Set comment for entire archive
   | DeleteArchiveComment
+    -- ^ Delete comment of entire archive
 
 -- | Determine target entry of action.
 
 targetEntry :: PendingAction -> Maybe EntrySelector
-targetEntry = undefined
+targetEntry (AddEntry       _ _ s) = Just s
+targetEntry (SinkEntry      _ _ s) = Just s
+targetEntry (LoadEntry      _ _ s) = Just s
+targetEntry (CopyEntry        _ s) = Just s
+targetEntry (RenameEntry      s _) = Just s
+targetEntry (DeleteEntry        s) = Just s
+targetEntry (Recompress       _ s) = Just s
+targetEntry (SetEntryComment  _ s) = Just s
+targetEntry (DeleteEntryComment s) = Just s
+targetEntry (SetModTime       _ s) = Just s
+targetEntry (AddExtraField    _ s) = Just s
+targetEntry (DeleteExtraField _ s) = Just s
+targetEntry (SetArchiveComment  _) = Nothing
+targetEntry DeleteArchiveComment   = Nothing
 
--- | Throws 'MultiDiskArchive'.
+-- | Scan central directory of an archive and return its description
+-- 'ArchiveDescription' as well as collection of its entries.
+--
+-- Throws 'MultiDiskArchive'.
 
 scanArchive
-  :: Path Abs File
+  :: Path Abs File     -- ^ Path to archive to scan
   -> IO (ArchiveDescription, Map EntrySelector EntryDescription)
 scanArchive = undefined
 
+-- | Given location of archive and information about specific archive entry
+-- 'EntryDescription', return its contents as strict 'ByteString'. Returned
+-- binary data is decompressed.
+
 getEntry
-  :: Path Abs File
-  -> EntryDescription
-  -> IO ByteString
+  :: Path Abs File     -- ^ Path to archive that contains the entry
+  -> EntryDescription  -- ^ Information needed to extract entry of interest
+  -> IO ByteString     -- ^ Decompressed binary data
 getEntry = undefined
 
+-- | The same as 'getEntry', but archive contents are not returned directly,
+-- but instead are streamed to given 'Sink'.
+
 sourceEntry
-  :: Path Abs File
-  -> EntryDescription
-  -> Sink ByteString (ResourceT IO) a
+  :: Path Abs File     -- ^ Path to archive that contains the entry
+  -> EntryDescription  -- ^ Information needed to extract entry of interest
+  -> Sink ByteString (ResourceT IO) a -- ^ Where to stream decompressed data
   -> IO a
 sourceEntry = undefined
 
+-- | Transform given pending actions so they can be performed efficiently in
+-- one pass. The action also prepares environment when necessary
+-- (e.g. creates temporary files).
+
 withOptimizedActions
-  :: Path Abs File
-  -> ArchiveDescription -- ???
-  -> Seq PendingAction
-  -> (Seq PendingAction -> IO ())
+  :: Path Abs File     -- ^ Location of archive file to edit or create
+  -> ArchiveDescription -- ^ Archive description
+  -> Seq PendingAction -- ^ Collection of pending actions
+  -> (Path Abs File -> Seq PendingAction -> IO ())
+     -- ^ Given name of file where to write archive and optimized actions,
+     -- do it
   -> IO ()
 withOptimizedActions = undefined
 
-commit :: Seq PendingAction -> IO ()
+-- | Undertake /all/ actions specified in the first argument of the
+-- function.
+
+commit
+  :: Path Abs File     -- ^ Where to write the new archive
+  -> Seq PendingAction -- ^ Collection of actions (should be optimized)
+  -> IO ()
 commit = undefined
 
 ----------------------------------------------------------------------------
